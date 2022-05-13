847e0fb3c9bf96c491282296853a5c5b1d3e4f7a
sang-w0o <robbyra@gmail.com>
2022-05-13 17:24:08 +0900

    arrow function 단순화

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
-export const HomePrivateRoute = ({children}) => {
-    console.log(children);
-    return localStorage.getItem('tokenInfo') ? <Navigate to = "/mainpage" replace/> : children;
-}
+export const HomePrivateRoute = ({ children }) =>
+  localStorage.getItem("tokenInfo") ? (
+    <Navigate to="/mainpage" replace />
+  ) : (
+    children
+  );
 
-export const PrivateRoute = ({children}) => {
-    console.log(children);
-    return localStorage.getItem('tokenInfo') ? children : <Navigate to = "/" replace/>;
-}
+export const PrivateRoute = ({ children }) =>
+  localStorage.getItem("tokenInfo") ? children : <Navigate to="/" replace />;
