9d71b5e916bb84b53f4a298660fb7c2e6ea0f37a
sang-w0o <robbyra@gmail.com>
2022-05-13 17:20:29 +0900

    console.log 제거 및 data 처리

--- src/Auth.js
+++ src/Auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { useNavigate} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import {setUser,setToken} from './redux/action';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser, setToken } from "./redux/action";
 const baseUrl = "https://test.chll.it";
 
 const Auth = () => {
@@ -9,29 +9,30 @@ const Auth = () => {
   const dispatch = useDispatch();
 
   const getToken = async () => {
-    
     const tokenInfo = {
-      accessToken : new URL(window.location.href).searchParams.get("accessToken"),
-      refreshToken : new URL(window.location.href).searchParams.get("refreshToken")
+      accessToken: new URL(window.location.href).searchParams.get(
+        "accessToken"
+      ),
+      refreshToken: new URL(window.location.href).searchParams.get(
+        "refreshToken"
+      ),
     };
-    
+
     dispatch(setToken(tokenInfo));
 
     try {
-      axios.get(`${baseUrl}/v1/user`, {
-        headers: {
-          Authorization: `Bearer ${tokenInfo.accessToken}`
-        }
-      }).then(response => {
-        response = response.data;
-        
-        console.log(response);
-        // localStorage or State 저장
-        console.log(response);
-        dispatch(setUser(response));
-        navigate('/mainpage');
-      });
-    } catch (error) { }
+      axios
+        .get(`${baseUrl}/v1/user`, {
+          headers: {
+            Authorization: `Bearer ${tokenInfo.accessToken}`,
+          },
+        })
+        .then((response) => {
+          const { data } = response;
+          dispatch(setUser(data));
+          navigate("/mainpage");
+        });
+    } catch (error) {}
   };
 
   getToken();
