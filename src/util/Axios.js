7855cf59eb51c9a558e1f0000fbb1c74664885aa
김민 <jangofett@naver.com>
2022-05-24 23:26:31 +0900

    feat:posting comment

--- src/util/Axios.js
+++ src/util/Axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const baseURL = "https://test.chll.it";
-const check = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInByb3ZpZGVyIjoidGVzdHVzZXIiLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjUyODAxNDY4LCJleHAiOjE2NTc4ODc4Njh9.qvR3b6Alms5tAuvB7_Ryr9e3BHKFMVTY-gnnHoEiYw4nQ8EpZtY3UFZhVSlMxlnWZQjNl36peK1g4zJdP-or5g"
+const check =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInByb3ZpZGVyIjoidGVzdHVzZXIiLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjUyODAxNDY4LCJleHAiOjE2NTc4ODc4Njh9.qvR3b6Alms5tAuvB7_Ryr9e3BHKFMVTY-gnnHoEiYw4nQ8EpZtY3UFZhVSlMxlnWZQjNl36peK1g4zJdP-or5g";
 
 export const Axios = axios.create({
   baseURL,
@@ -38,17 +39,19 @@ export const BoardCommentGetApi = async (boardId) => {
   });
 };
 
-export const BoardCommentPostApi = async(boardId , comment) =>{
-  return Axios.post(`/v1/board/${boardId}/comment`,
-  {
-    content : comment
-  },
-  {
-    headers: {
-      Authorization: `Bearer ${
-        check
-        //JSON.parse(localStorage.getItem("tokenInfo")).accessToken
-      }`,
-    },    
-  });
+export const BoardCommentPostApi = async (boardId, comment) => {
+  return Axios.post(
+    `/v1/board/${boardId}/comment`,
+    {
+      content: comment,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${
+          check
+          //JSON.parse(localStorage.getItem("tokenInfo")).accessToken
+        }`,
+      },
+    },
+  );
 };
