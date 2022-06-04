4c1f024856f3786d097ab2a9edd021c9890553d7
0chil <0@chll.it>
2022-06-04 15:43:51 +0900

    fix: remove token

--- src/util/Axios.js
+++ src/util/Axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const baseURL = "https://test.chll.it";
-const test =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInByb3ZpZGVyIjoidGVzdHVzZXIiLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjUyODAxNDY4LCJleHAiOjE2NTc4ODc4Njh9.qvR3b6Alms5tAuvB7_Ryr9e3BHKFMVTY-gnnHoEiYw4nQ8EpZtY3UFZhVSlMxlnWZQjNl36peK1g4zJdP-or5g";
 
 export const Axios = axios.create({
   baseURL,
