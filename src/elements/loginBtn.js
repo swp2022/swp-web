97dde11d406755c54eb691ec6eddacf7d6f4e4ff
0chil <0@chll.it>
2022-06-06 22:55:05 +0900

    chore: change domain to ostudy.cf

--- src/elements/loginBtn.js
+++ src/elements/loginBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./loginBtn.css";
 
-const KAKAO_AUTH_URL = `https://test.chll.it/oauth2/authorization/kakao?redirect-uri=${window.location.protocol}//${window.location.host}/login/oauth2/code/kakao`;
+const KAKAO_AUTH_URL = `https://api.ostudy.cf/oauth2/authorization/kakao?redirect-uri=${window.location.protocol}//${window.location.host}/login/oauth2/code/kakao`;
 
 const loginBtn = () => (
   <div className="login-btn">
